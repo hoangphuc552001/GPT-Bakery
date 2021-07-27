47d379435f8606b1f89c2911a1f09f148a950f0e
hoangphuc552001 <67599931+hoangphuc552001@users.noreply.github.com>
2021-07-27 23:02:02 +0700

    Initial commit

---
+++ app/src/main/java/com/test/gpt_bakery/Loginphone.java
@@ -0,0 +1,14 @@
+package com.test.gpt_bakery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Loginphone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginphone);
+    }
+}
